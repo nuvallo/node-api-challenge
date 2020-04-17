b4a6b688557439ecdfdcc8a839bfe28546c89f17
nuvallo <nuvallo.inc@gmail.com>
2020-04-17 19:22:13 -0400

    GET actions complete

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const actionDB = require("../data/helpers/actionModel");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  actionDB
+    .get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+      res.status(500).json({
+        message: "Error getting actions",
+      });
+    });
+});
 
 module.exports = router;
